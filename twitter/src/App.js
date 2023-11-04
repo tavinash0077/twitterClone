8aecbd21a4b046053ab4ba3c831e63a88fc8742c
tavinash0077 <tavinash0077@gmail.com>
2023-11-04 19:12:38 +0530

    pusg1

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
+import Authentication from './Components/Authentication/Authentication';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={true ? <HomePage /> : <Authentication />}>
+
+        </Route>
+      </Routes>
     </div>
   );
 }
